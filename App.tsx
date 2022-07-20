379447cdae969b5d9f18625bc1711972f3654047
Jair Araujo <jair.jay@hotmail.com>
2022-07-19 22:24:53 -0300

    Call list created

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
-import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
-import {THEME} from './src/styles/theme';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
+import { THEME } from './src/styles/theme';
 import { SignIn } from './src/screens/SignIn';
+import { Home } from './src/screens/Home';
 import { Loading } from './src/components/Loading';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar
-      barStyle="light-content"
-      backgroundColor="transparent"
-      translucent
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
       />
-      {fontsLoaded ? <SignIn/> : <Loading/>}
+      {fontsLoaded ? <Home /> : <Loading />}
     </NativeBaseProvider>
   );
 }
-
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -5,19 +5,25 @@ import {
   IconButton,
   useTheme,
   Text,
-  Heading
+  Heading,
+  FlatList,
+  Center
 } from 'native-base';
 
-import { SignOut } from 'phosphor-react-native';
+import { SignOut, ChatTeardropText } from 'phosphor-react-native';
 
 import Logo from '../assets/logo_secondary.svg';
 
 import { Filter } from '../components/Filter';
+import { Button } from '../components/Button';
+import { Order, OrderProps } from '../components/Order';
+import { color } from 'native-base/lib/typescript/theme/styled-system';
 
 export function Home() {
   const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>(
     'open'
   );
+  const [orders, setOrders] = useState<OrderProps[]>([]);
 
   const { colors } = useTheme();
 
@@ -59,11 +65,30 @@ export function Home() {
 
           <Filter
             type="closed"
-            title="Finalizado"
+            title="Finalizados"
             onPress={() => setStatusSelected('closed')}
             isActive={statusSelected === 'closed'}
           />
         </HStack>
+
+        <FlatList
+          data={orders}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <Order data={item} />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 100 }}
+          ListEmptyComponent={() => (
+            <Center>
+              <ChatTeardropText color={colors.gray[300]} size={40} />
+              <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
+                Você ainda não possui {'\n'}
+                solicitações{' '}
+                {statusSelected === 'open' ? 'em andamento' : 'finalizados'}
+              </Text>
+            </Center>
+          )}
+        />
+        <Button title="Nova Solicitação" />
       </VStack>
     </VStack>
   );
